3b8c93e4d3873f87a5817f596528e864fe4f2699
nobe4 <haffreinguev@gmail.com>
2014-03-28 19:00:11 +0100

    Added the table for the grades with random values but good column header

--- java/src/View/Main_Page.java
+++ java/src/View/Main_Page.java
@@ -2,20 +2,35 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by padawan on 3/28/14.
  */
 public class Main_Page extends JFrame {
-    private Login_Page t;
+    private Login_Page login_page;
+    private Student_Page student_page;
 
     public Main_Page() throws HeadlessException {
         this.setTitle("Login");
 
-        t = new Login_Page();
+        login_page = new Login_Page();
+        student_page = new Student_Page();
 
-        this.setContentPane(t.getMainPanel());
-        this.setSize(200,200);
+        this.setContentPane(student_page.getMainPanel());
+        this.setSize(500,500);
         this.setVisible(true);
+
+        this.addListeners();
+    }
+
+    public void addListeners(){
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
--- java/src/View/Student_Page.java
+++ java/src/View/Student_Page.java
@@ -1,7 +1,87 @@
 package View;
 
+import javax.swing.*;
+
 /**
  * Created by padawan on 3/26/14.
  */
 public class Student_Page {
+    private JTabbedPane tabbedPane1;
+    private JPanel mainPanel;
+    private JTable gradesTable;
+    private JList alertList;
+
+    public Student_Page() {
+        displayGrades();
+    }
+
+    public JPanel getMainPanel() {return mainPanel;}
+
+    public void displayGrades(){
+    }
+
+    private void createUIComponents() {
+        String[] columnNames = {"Id", "Module", "Teacher", "Grade"};
+        Object[][] data = {
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5},
+                {2,3,4,5}
+        };
+        gradesTable = new JTable(data, columnNames);
+        gradesTable.setFillsViewportHeight(true);
+    }
 }
