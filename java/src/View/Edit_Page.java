85549a716ec79f908aeefd308c514cb11b539eeb
nobe4 <haffreinguev@gmail.com>
2014-04-04 09:26:21 +0200

    finish the edit page and add a tab selection on teacher page button click

--- java/src/View/Edit_Page.java
+++ java/src/View/Edit_Page.java
@@ -6,22 +6,75 @@ import javax.swing.*;
  * Created by padawan on 4/2/14.
  */
 public class Edit_Page {
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane mainPane;
     private JPanel mainPanel;
+
     private JTextField moduleNameInput;
     private JComboBox moduleTeacherChoice;
     private JComboBox moduleYearChoice;
-    private JButton addModuleButton;
-    private JTextField mailField;
-    private JPanel teacherChoice_Module;
+    private JButton moduleButton;
+
     private JTextField studentNameInput;
     private JTextField studentMailInput;
     private JTextField studentPasswordInput;
     private JComboBox studentSpecialityChoice;
     private JComboBox studentYearChoice;
-    private JButton studentAddButton;
+    private JButton studentButton;
+
+    private JTextField gradeValueInput;
+    private JTextField gradeCoefInput;
+    private JComboBox gradeStudentChoice;
+    private JComboBox gradeModuleChoice;
+    private JComboBox gradeTypeChoice;
+    private JButton gradeButton;
+
+    private JComboBox assistantStudentChoice;
+    private JComboBox assistantTeacherChoice;
+    private JButton assistantButton;
+
+    private JComboBox tutorTeacherChoice;
+    private JComboBox tutorModuleChoice;
+    private JButton tutorButton;
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public JTabbedPane getMainPane() {
+        return mainPane;
+    }
+
+    public void setOptions(int tab, String text) {
+        switch (tab) {
+            case 0:
+                moduleButton.setText(text);
+                break;
+            case 1:
+                studentButton.setText(text);
+                break;
+            case 2:
+                gradeButton.setText(text);
+                break;
+            case 3:
+                assistantButton.setText(text);
+                break;
+            case 4:
+                tutorButton.setText(text);
+                break;
+
+        }
+
+        if (text.equals("Add"))
+            emptyFields(tab);
+        else
+            fillFields(tab);
+    }
+
+    private void fillFields(int tab) {
+        System.out.println("Fill fields for tab " + String.valueOf(tab));
+    }
+
+    private void emptyFields(int tab) {
+        System.out.println("Empty fields for tab " + String.valueOf(tab));
     }
 }
--- java/src/View/Teacher_Page.java
+++ java/src/View/Teacher_Page.java
@@ -1,9 +1,26 @@
 package View;
 
 
+import Controller.Database_Controller;
+import Controller.Main_Controller;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Teacher_Page {
+
+    private enum TAB {MODULE, STUDENT, GRADE, ASSISTANT, TUTOR}
+
+    ;
+
+    private enum ACTION {ADD, SET, DELETE}
+
+    ;
+
     private JTabbedPane tabbedPane1;
 
     public JPanel getMainPanel() {
@@ -23,4 +40,82 @@ public class Teacher_Page {
     private JButton newAssistantButton;
     private JButton newModuleButton;
     private JButton newGradeButton;
+
+    private JTable alertTable;
+
+    private JFrame editFrame;
+    private Edit_Page edit_page = new Edit_Page();
+
+    // database attributes
+    private Main_Controller mainController;
+
+
+    public Teacher_Page(Database_Controller dc) {
+
+        // add the main controller to handle collections
+        mainController = dc.getMainController();
+
+
+        editFrame = new JFrame();
+        editFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                editFrame.setVisible(false);
+            }
+        });
+        editFrame.setTitle("Edit");
+        editFrame.setSize(500, 500);
+        editFrame.setMinimumSize(new Dimension(400, 400));
+        editFrame.setContentPane(edit_page.getMainPanel());
+
+        this.addListener();
+    }
+
+    public void addListener() {
+        newModuleButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                editFrame.setVisible(true);
+                edit_page.getMainPane().setSelectedIndex(0);
+                edit_page.setOptions(0, "Add");
+            }
+        });
+        newStudentButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                editFrame.setVisible(true);
+                edit_page.getMainPane().setSelectedIndex(1);
+                edit_page.setOptions(1, "Add");
+            }
+        });
+        newGradeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                editFrame.setVisible(true);
+                edit_page.getMainPane().setSelectedIndex(2);
+                edit_page.setOptions(2, "Add");
+            }
+        });
+        newAssistantButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                editFrame.setVisible(true);
+                edit_page.getMainPane().setSelectedIndex(0);
+                edit_page.setOptions(3, "Add");
+            }
+        });
+        newTutorButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                editFrame.setVisible(true);
+                edit_page.getMainPane().setSelectedIndex(0);
+                edit_page.setOptions(4, "Add");
+            }
+        });
+    }
+
+
+    public void fillTables() {
+
+    }
 }
